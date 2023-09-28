4678f62d94aceb9f0d6da9259528c1febe71773a
Marcelo Costa da Mata <mcosta.matta@gmail.com>
2023-09-28 15:06:10 -0300

    Aplicação da lógica do jogo

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Criação da página HTML e CSS - Finalizadas
 Script em desenvolvimento - Apresentando erro. No aguardo da resposta do professor do curso
 Resolução do problema. Ajuste de variáveis
 Desenvolvimento do script (JS). Não aparece mensagem no console conforme exemplo do professor. Aparentemente, o código está correto.
+Aplicação da lógica do jogo, mas não está mostrando as mensagens
--- js/scripts.js
+++ js/scripts.js
@@ -66,12 +66,12 @@ function checkinCondition() {
         let b2Child = b2.childNodes[0].className
         let b3Child = b3.childNodes[0].className
 
-        if (b1Child == "X" && b2Child == "X" && b3Child == "X") {
+        if (b1Child == "x" && b2Child == "x" && b3Child == "x") {
             // x
-            console.log("x venceu")
+            declareWinner("x")
         } else if (b1Child == "o" && b2Child == "o" && b3Child == "o") {
             // o
-            console.log("o venceu")
+            declareWinner("o")
         }
     }
 
@@ -81,12 +81,12 @@ function checkinCondition() {
         let b5Child = b5.childNodes[0].className
         let b6Child = b6.childNodes[0].className
 
-        if (b4Child == "X" && b5Child == "X" && b6Child == "X") {
+        if (b4Child == "x" && b5Child == "x" && b6Child == "x") {
             // x
-            console.log("x venceu")
+            declareWinner("x")
         } else if (b4Child == "o" && b5Child == "o" && b6Child == "o") {
             // o
-            console.log("o venceu")
+            declareWinner("o")
         }
     }
 
@@ -96,12 +96,125 @@ function checkinCondition() {
         let b8Child = b8.childNodes[0].className
         let b9Child = b9.childNodes[0].className
 
-        if (b7Child == "X" && b8Child == "X" && b9Child == "X") {
+        if (b7Child == "x" && b8Child == "x" && b9Child == "x") {
             // x
-            console.log("x venceu")
+            declareWinner("x")
         } else if (b7Child == "o" && b8Child == "o" && b9Child == "o") {
             // o
-            console.log("o venceu")
+            declareWinner("o")
         }
     }
+
+    // Jogadas na vertical
+    if (b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0) {
+
+        let b1Child = b1.childNodes[0].className
+        let b4Child = b4.childNodes[0].className
+        let b7Child = b7.childNodes[0].className
+
+        if (b1Child == "x" && b4Child == "x" && b7Child == "x") {
+            // x
+            declareWinner("x")
+        } else if (b1Child == "o" && b4Child == "o" && b7Child == "o") {
+            // o
+            declareWinner("o")
+        }
+    }
+
+    if (b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0) {
+
+        let b2Child = b2.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b8Child = b8.childNodes[0].className
+
+        if (b2Child == "x" && b5Child == "x" && b8Child == "x") {
+            // x
+            declareWinner("x")
+        } else if (b2Child == "o" && b5Child == "o" && b8Child == "o") {
+            // o
+            declareWinner("o")
+        }
+    }
+
+    if (b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0) {
+
+        let b3Child = b3.childNodes[0].className
+        let b6Child = b6.childNodes[0].className
+        let b9Child = b9.childNodes[0].className
+
+        if (b3Child == "x" && b6Child == "x" && b9Child == "x") {
+            // x
+            declareWinner("x")
+        } else if (b3Child == "o" && b6Child == "o" && b9Child == "o") {
+            // o
+            declareWinner("o")
+        }
+    }
+
+    // Diagonal
+    if (b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0) {
+
+        let b1Child = b1.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b9Child = b9.childNodes[0].className
+
+        if (b1Child == "x" && b5Child == "x" && b9Child == "x") {
+            // x
+            declareWinner("x")
+        } else if (b1Child == "o" && b5Child == "o" && b9Child == "o") {
+            // o
+            declareWinner("o")
+        }
+    }
+
+    if (b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0) {
+
+        let b3Child = b3.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b7Child = b7.childNodes[0].className
+
+        if (b3Child == "x" && b5Child == "x" && b7Child == "x") {
+            // x
+            declareWinner("x")
+        } else if (b3Child == "o" && b5Child == "o" && b7Child == "o") {
+            // o
+            declareWinner("o")
+        }
+    }
+
+    // Em caso do jogo dar 'velha'
+    let counter = 0
+
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i].childNodes[0] != undefined) {
+            counter++
+        }
+    }
+
+    if (counter == 9) {
+        declareWinner("Deu velha")
+    }
+}
+
+
+// Lógica para avaliar o jogo, declarar o vencedor, e limpar o jogo
+function declareWinner(winner) {
+
+    let scoreboardX = document.querySelector("#scoreboard-1")
+    let scoreboardY = document.querySelector("#scoreboard-2")
+    let msg = "";
+
+    if (winner == "x") {
+        scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1
+        msg = "O jogador 1 venceu"
+    } else if (winner == "y") {
+        scoreboardY.textContent = parseInt(scoreboardY.textContent) + 1
+        msg = "O jogador 2 venceu"
+    } else {
+        msg = "Deu velha"
+    }
+
+    // Exibir mensagem
+    messageText.innerHTML = msg
+    messageContainer.classList.remove("hide")
 }
